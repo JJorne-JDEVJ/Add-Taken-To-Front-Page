28caaa5f961c28b693c008114a79feebe8089b99
JJorne-JDEVJ <159343253+JJorne-JDEVJ@users.noreply.github.com>
2024-06-09 13:45:11 +0200

    actiualy do somthing

--- Taken.js
+++ Taken.js
@@ -1,8 +1,16 @@
 // nog niet klaar...
 
 function fetchDataAndFilterDeadlines() {
-  const url = 'https://{school}.smartschool.be/planner/api/v1/planned-elements/user/{user}?from={date}&to={date+1week}';
-
+  const currentUrl = window.location.href;
+  const school_name = currentUrl.split("/")[2];
+  const plannerUrl = document.getElementById('datePickerMenu').getAttribute('plannerurl');
+  const user = plannerUrl.split("/")[4]);
+  const date = await getDateInCorrectFormat(true, false)
+  const endDate = await getDateInCorrectFormat(true, true
+                                              )
+  const url = `https://${school_name}.smartschool.be/planner/api/v1/planned-elements/user/${user}?from=${date}&to=${endDate}`;
+  console.log(url);
+  
   fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -22,5 +30,33 @@ function fetchDataAndFilterDeadlines() {
     });
 }
 
-// Call the function to fetch data and filter deadlines
-fetchDataAndFilterDeadlines();
+async function getDateInCorrectFormat(isFancyFormat, plusOneWeek) {
+    if (plusOneWeek){
+      let currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + 7);
+    }else{
+        let currentDate = new Date();
+    }
+    if (currentDate.getHours() >= 18) {
+        currentDate.setDate(currentDate.getDate() + 3);
+        currentDate.setHours(7);
+        currentDate.setMinutes(30);
+    } else if (currentDate.getDay() === 5 && currentDate.getHours() >= 18) {
+        currentDate.setDate(currentDate.getDate() + 3);
+        currentDate.setHours(7);
+        currentDate.setMinutes(30);
+    } else if (currentDate.getDay() === 6 || currentDate.getDay() === 0) {
+        currentDate.setDate(currentDate.getDate() + (8 - currentDate.getDay()));
+        currentDate.setHours(7);
+        currentDate.setMinutes(30);
+    }
+
+    if (isFancyFormat) {
+        const day = currentDate.getDate().toString().padStart(2, '0');
+        const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+        const year = currentDate.getFullYear();
+        return `${year}-${month}-${day}`;
+    } else {
+        return currentDate;
+    }
+}
